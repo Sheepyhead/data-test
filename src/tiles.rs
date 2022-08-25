3b8c92210399f0bf824289ce688a2c2ff241491e
Troels Peter Jessen <trojes@tuta.io>
2022-08-25 22:09:42 +0200

    Implemented repeating tileset

--- src/tiles.rs
+++ src/tiles.rs
@@ -8,8 +8,9 @@ pub struct Tiles;
 impl Plugin for Tiles {
     fn build(&self, app: &mut App) {
         app.add_plugin(YamlAssetPlugin::<Tile>::new(&["tile"]))
+            .add_plugin(YamlAssetPlugin::<RepeatingTileSet>::new(&["tileset"]))
             .add_startup_system(load)
-            .add_system(spawn)
+            .add_system(spawn_tileset)
             .add_system(spawn_gltf);
     }
 }
@@ -21,11 +22,13 @@ pub struct Tile {
     position: BTreeMap<String, i32>,
 }
 
-struct TileSet(Vec<Handle<Tile>>);
+// struct TileSet(Vec<Handle<Tile>>);
+
+struct TileSets(Vec<Handle<RepeatingTileSet>>);
 
 fn load(mut commands: Commands, ass: Res<AssetServer>) {
-    let grass_tile = ass.load("grasstile.tile");
-    commands.insert_resource(TileSet(vec![grass_tile]));
+    let grass_set = ass.load("grasstileset.tileset");
+    commands.insert_resource(TileSets(vec![grass_set]));
 }
 
 #[derive(Component, Deref)]
@@ -40,18 +43,26 @@ fn spawn(
     for ev in events.iter() {
         if let AssetEvent::Created { handle } = ev {
             let tile = tiles.get(handle).unwrap();
-            commands
-                .spawn_bundle(SpatialBundle {
-                    transform: Transform::from_xyz(
-                        *tile.position.get("x").unwrap() as f32,
-                        0.0,
-                        *tile.position.get("y").unwrap() as f32,
-                    ),
-                    ..default()
-                })
-                .insert(SpawnGltfScene(ass.load(&tile.model)));
+            spawn_tile(
+                &tile.model,
+                IVec2 {
+                    x: *tile.position.get("x").unwrap(),
+                    y: *tile.position.get("y").unwrap(),
+                },
+                &mut commands,
+                &ass,
+            );
         }
     }
+}
+
+fn spawn_tile(model: &String, pos: IVec2, commands: &mut Commands, ass: &Res<AssetServer>) {
+    commands
+        .spawn_bundle(SpatialBundle {
+            transform: Transform::from_xyz(pos.x as f32, 0.0, pos.y as f32),
+            ..default()
+        })
+        .insert(SpawnGltfScene(ass.load(model)));
 }
 
 fn spawn_gltf(
@@ -65,6 +76,32 @@ fn spawn_gltf(
                 .entity(entity)
                 .insert(gltf.scenes[0].clone())
                 .remove::<SpawnGltfScene>();
+        }
+    }
+}
+
+#[derive(serde::Deserialize, TypeUuid)]
+#[uuid = "688ebe3a-8d7f-4658-b945-f408c1370ba8"]
+struct RepeatingTileSet {
+    model: String,
+    width: i32,
+    height: i32,
+}
+
+fn spawn_tileset(
+    mut commands: Commands,
+    ass: Res<AssetServer>,
+    mut events: EventReader<AssetEvent<RepeatingTileSet>>,
+    sets: Res<Assets<RepeatingTileSet>>,
+) {
+    for ev in events.iter() {
+        if let AssetEvent::Created { handle } = ev {
+            let set = sets.get(handle).unwrap();
+            for x in 0..set.width {
+                for y in 0..set.height {
+                    spawn_tile(&set.model, IVec2 { x, y }, &mut commands, &ass);
+                }
+            }
         }
     }
 }

8798675ba3b965dd03cf887b3fc7a722f4b48fa7
Troels Peter Jessen <trojes@tuta.io>
2022-09-06 21:16:42 +0200

    Implemented character animation mvp

--- src/player.rs
+++ src/player.rs
@@ -2,7 +2,7 @@ use bevy::{prelude::*, reflect::TypeUuid};
 use bevy_common_assets::yaml::YamlAssetPlugin;
 use bevy_rapier3d::prelude::*;
 
-use crate::gltf::SpawnGltfScene;
+use crate::{gltf::SpawnGltfScene, movement::Destination};
 
 pub struct PlayerPlugin;
 
@@ -11,7 +11,11 @@ impl Plugin for PlayerPlugin {
         app.add_plugin(YamlAssetPlugin::<PlayerData>::new(&["player"]))
             .add_startup_system(load)
             .add_system(spawn)
-            .add_system(initial_animate_character);
+            .add_system(initial_set_animation)
+            .add_system(set_animation)
+            .add_system(setup_animation_player)
+            .add_system(start_running)
+            .add_system_to_stage(CoreStage::PostUpdate, stop_running);
     }
 }
 
@@ -40,33 +44,87 @@ fn spawn(
                 scene: ass.load(&data.model).into(),
                 animations: CharacterAnimations {
                     idle: ass.load(&format!("{}#Animation0", data.idle_animation)),
+                    run: ass.load(&format!("{}#Animation0", data.run_animation)),
                 },
+                animation_state: CharacterAnimationState::Idle,
                 collider: Collider::cylinder(1.0, 0.5),
                 rb: RigidBody::Dynamic,
-                la: LockedAxes::TRANSLATION_LOCKED_Y
-                    | LockedAxes::ROTATION_LOCKED,
+                la: LockedAxes::TRANSLATION_LOCKED_Y | LockedAxes::ROTATION_LOCKED,
                 player: Player,
             });
         }
     }
 }
 
-fn initial_animate_character(
-    mut players: Query<(&Parent, &mut AnimationPlayer), Added<AnimationPlayer>>,
-    parents: Query<&Parent, Without<AnimationPlayer>>,
-    animations: Query<&CharacterAnimations, Without<AnimationPlayer>>,
+fn initial_set_animation(
+    characters: Query<
+        (
+            &CharacterAnimations,
+            &AnimationPlayerReference,
+            &CharacterAnimationState,
+        ),
+        Added<AnimationPlayerReference>,
+    >,
+    mut players: Query<&mut AnimationPlayer, Without<AnimationPlayerReference>>,
 ) {
-    for (mut parent, mut player) in players.iter_mut() {
-        loop {
-            if let Ok(animations) = animations.get(parent.get()) {
-                player.play(animations.idle.clone()).repeat();
-                break;
-            } else if let Ok(new_parent) = parents.get(parent.get()) {
-                parent = new_parent;
-            } else {
-                break;
-            }
+    for (animations, AnimationPlayerReference(player), state) in characters.iter() {
+        players
+            .get_mut(*player)
+            .unwrap()
+            .play(animations.get_handle(state))
+            .repeat();
+    }
+}
+
+fn set_animation(
+    characters: Query<
+        (
+            &CharacterAnimations,
+            &AnimationPlayerReference,
+            &CharacterAnimationState,
+        ),
+        Changed<CharacterAnimationState>,
+    >,
+    mut players: Query<&mut AnimationPlayer, Without<AnimationPlayerReference>>,
+) {
+    for (animations, AnimationPlayerReference(player), state) in characters.iter() {
+        players
+            .get_mut(*player)
+            .unwrap()
+            .play(animations.get_handle(state))
+            .repeat();
+    }
+}
+
+fn start_running(mut characters: Query<&mut CharacterAnimationState, Added<Destination>>) {
+    for mut character in characters.iter_mut() {
+        *character = CharacterAnimationState::Running;
+    }
+}
+
+fn stop_running(
+    removed: RemovedComponents<Destination>,
+    mut characters: Query<&mut CharacterAnimationState>,
+) {
+    for entity in removed.iter() {
+        if let Ok(mut state) = characters.get_mut(entity) {
+            *state = CharacterAnimationState::Idle;
         }
+    }
+}
+
+#[derive(Component)]
+struct AnimationPlayerReference(Entity);
+
+fn setup_animation_player(
+    mut commands: Commands,
+    player: Query<(Entity, &Parent), Added<AnimationPlayer>>,
+    parents: Query<&Parent, Without<AnimationPlayer>>,
+) {
+    for (player, player_parent) in player.iter() {
+        commands
+            .entity(**parents.get(**player_parent).unwrap())
+            .insert(AnimationPlayerReference(player));
     }
 }
 
@@ -75,6 +133,7 @@ fn initial_animate_character(
 struct PlayerData {
     model: String,
     idle_animation: String,
+    run_animation: String,
 }
 
 #[derive(Bundle)]
@@ -83,6 +142,7 @@ struct PlayerBundle {
     spatial: SpatialBundle,
     scene: SpawnGltfScene,
     animations: CharacterAnimations,
+    animation_state: CharacterAnimationState,
     collider: Collider,
     rb: RigidBody,
     la: LockedAxes,
@@ -90,8 +150,24 @@ struct PlayerBundle {
 }
 
 #[derive(Component)]
-struct CharacterAnimations {
+pub struct CharacterAnimations {
     idle: Handle<AnimationClip>,
+    run: Handle<AnimationClip>,
+}
+
+impl CharacterAnimations {
+    fn get_handle(&self, state: &CharacterAnimationState) -> Handle<AnimationClip> {
+        match state {
+            CharacterAnimationState::Idle => self.idle.clone(),
+            CharacterAnimationState::Running => self.run.clone(),
+        }
+    }
+}
+
+#[derive(Component)]
+enum CharacterAnimationState {
+    Idle,
+    Running,
 }
 
 #[derive(Component)]
